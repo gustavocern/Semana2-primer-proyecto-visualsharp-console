56c6c230e5fae88b8a4af46c378691ade4e26ee7
MINEDUCYT <MINEDUCYT@DESKTOP-NFDMETJ>
2022-07-25 12:54:00 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2_primer_proyecto_visualsharp_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
