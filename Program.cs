f7b589ea9ce8ef515c07c15b2ad1f6a2f565e265
gustavocern <gustavoargueta363@gmail.com>
2022-07-25 20:29:29 -0500

    Termine de  cambia el idioma al codigo.

--- Program.cs
+++ Program.cs
@@ -17,43 +17,43 @@ namespace Semana2_primer_proyecto_visualsharp_console
             int num1 = 0; int num2 = 0;
 
             // Display title as the C# console calculator app.
-            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("Console Calculadora en C#\r");
             Console.WriteLine("------------------------\n");
 
             // Ask the user to type the first number.
-            Console.WriteLine("Type a number, and then press Enter");
+            Console.WriteLine("Digite el numero, Precione ENTER");
             num1 = Convert.ToInt32(Console.ReadLine());
 
             // Ask the user to type the second number.
-            Console.WriteLine("Type another number, and then press Enter");
+            Console.WriteLine("Digite el numero, Precione ENTER");
             num2 = Convert.ToInt32(Console.ReadLine());
 
             // Ask the user to choose an option.
-            Console.WriteLine("Choose an option from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
+            Console.WriteLine("Seleccione la opcion que desea relalizar:");
+            Console.WriteLine("\ts - Suma");
+            Console.WriteLine("\tr - Resta");
+            Console.WriteLine("\tm - Multiplicacion");
+            Console.WriteLine("\td - Divicion");
+            Console.Write("cual opcion? ");
 
             // Use a switch statement to do the math.
             switch (Console.ReadLine())
             {
-                case "a":
-                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-                    break;
                 case "s":
-                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    Console.WriteLine($"El resultado es : {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "r":
+                    Console.WriteLine($"El resultado es : {num1} - {num2} = " + (num1 - num2));
                     break;
                 case "m":
-                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    Console.WriteLine($"El resultado es : {num1} * {num2} = " + (num1 * num2));
                     break;
                 case "d":
-                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    Console.WriteLine($"El resultado es : {num1} / {num2} = " + (num1 / num2));
                     break;
             }
             // Wait for the user to respond before closing.
-            Console.Write("Press any key to close the Calculator console app...");
+            Console.Write("Presione ENTER para cerrar la consola");
             Console.ReadKey();
         }
     }
