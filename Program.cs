e3feef7c56346497a8b600f82c626198ad3cb2e6
gustavocern <gustavoargueta363@gmail.com>
2022-07-25 20:34:32 -0500

    Termine de editar el codigo.

--- Program.cs
+++ Program.cs
@@ -13,22 +13,22 @@ namespace Semana2_primer_proyecto_visualsharp_console
     {
         static void Main(string[] args)
         {
-            // Declare variables and then initialize to zero.
+            
             int num1 = 0; int num2 = 0;
 
-            // Display title as the C# console calculator app.
+            
             Console.WriteLine("Console Calculadora en C#\r");
             Console.WriteLine("------------------------\n");
 
-            // Ask the user to type the first number.
+           
             Console.WriteLine("Digite el numero, Precione ENTER");
             num1 = Convert.ToInt32(Console.ReadLine());
 
-            // Ask the user to type the second number.
+           
             Console.WriteLine("Digite el numero, Precione ENTER");
             num2 = Convert.ToInt32(Console.ReadLine());
 
-            // Ask the user to choose an option.
+            
             Console.WriteLine("Seleccione la opcion que desea relalizar:");
             Console.WriteLine("\ts - Suma");
             Console.WriteLine("\tr - Resta");
@@ -36,7 +36,7 @@ namespace Semana2_primer_proyecto_visualsharp_console
             Console.WriteLine("\td - Divicion");
             Console.Write("cual opcion? ");
 
-            // Use a switch statement to do the math.
+           
             switch (Console.ReadLine())
             {
                 case "s":
@@ -52,7 +52,7 @@ namespace Semana2_primer_proyecto_visualsharp_console
                     Console.WriteLine($"El resultado es : {num1} / {num2} = " + (num1 / num2));
                     break;
             }
-            // Wait for the user to respond before closing.
+           
             Console.Write("Presione ENTER para cerrar la consola");
             Console.ReadKey();
         }
